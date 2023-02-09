5bcf15157e389691f178ed9fbad1f9e4bdb8d478
Danillo Soares <Danillohss@gmail.com>
2023-02-09 18:11:44 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header> Minhas tarefas </header>
+    <Task/>
+
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Task from './components/Task';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Task
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  header{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    font-size: 40px;
+    color: #FFF;
+  }
 </style>
